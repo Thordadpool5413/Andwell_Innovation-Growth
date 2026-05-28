9feceaa6dcbad8337aefeb789eb277d593bae7bc
Thordadpool5413 <nick.lynch.coaching@outlook.com>
2026-05-27 21:41:58 -0400

    Load visual enhancement stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import './visual-enhancements.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import StorageCleanup from './storage-cleanup';
